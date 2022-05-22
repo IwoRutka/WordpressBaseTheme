1e617e2aa977a7bb34981aa30f7e43e6d51a53d7
iwor <web@fotax.pl>
2022-05-22 16:20:51 +0200

    Template Parts & Page Templates Fixes #7

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@
 
 function load_css()
 {
-    wp_register_style( 'bootstrap', get_template_directory_uri().'/css/bootsrap.min.css', array(), false, 'all' );
+    wp_register_style( 'bootstrap', get_template_directory_uri().'/css/bootstrap.min.css', array(), false, 'all' );
     wp_enqueue_style( 'bootstrap');
 
 }
@@ -11,10 +11,11 @@ function load_css()
 add_action( 'wp_enqueue_scripts', 'load_css');
 
 
+
 function load_js()
 {
     wp_enqueue_script( 'jquery' );
-    wp_register_script( 'bootstrap', get_template_directory_uri().'/js/bootsrap.min.js', 'jquery', false, true);
+    wp_register_script( 'bootstrap', get_template_directory_uri().'/js/bootstrap.min.js', 'jquery', false, false);
     wp_enqueue_script( 'bootstrap' );
 
 }
