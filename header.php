7173bca2f0014761d729f35565332c11bc699062
iwor <web@fotax.pl>
2022-06-12 18:55:46 +0200

    SASS style Fixes #27

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
         ?>
     <nav class="navbar navbar-expand-md navbar-light bg-light">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">MyWpDev</a>
+        <a class="navbar-brand" href="/">MyWpDev</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
